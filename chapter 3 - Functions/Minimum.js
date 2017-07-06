322e2bb6a9d0ba96a7e2e8464b1f8845e82275e4
AssassinJL <joshualadiao@yahoo.com>
2017-07-06 09:01:07 +0800

    Create Minimum.js

---
+++ chapter 3 - Functions/Minimum.js
@@ -0,0 +1,7 @@
+function minimum(a, b) {
+	return a < b ? a : b;
+}
+console.log(minimum(6, 9));
+console.log(minimum("a", "A"));
+console.log(minimum(9.6, 6.1));
+console.log(minimum(true, false));
