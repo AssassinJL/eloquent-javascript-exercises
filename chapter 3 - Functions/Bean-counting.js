a36ebf91a8062e7b17d87192e2e0e87b305c6fab
AssassinJL <joshualadiao@yahoo.com>
2017-07-06 09:05:53 +0800

    Update and rename bean-counting.js to Bean-counting.js

---
+++ chapter 3 - Functions/Bean-counting.js
@@ -0,0 +1,35 @@
+function countBs(string) {
+    var i = 0, count = 0;
+    while (i < string.length) {
+	    if (string.charAt(i++) == "B") {
+		    ++count;
+	    }
+    }
+    return count;
+}
+console.log(countBs("BBteiBfdsfbbb"));
+
+function countChar(string, c) {
+    var i = 0, count = 0;
+    while (i < string.length) {
+        if (string.charAt(i++) == c) {
+            ++count;
+        }
+    }
+    return count;
+}
+console.log(countChar("bbBfBewyt", "B"));
+
+var s = "SfsdfSssfdggf";
+function countChar(string, len, count, c) { // recursive 
+    if (len < 0) {
+        return count;
+    }
+    else {
+        if (string.charAt(len) == c) {
+            ++count;
+        }
+        return countChar(string, --len, count, c)
+    }
+}
+console.log(countChar(s, s.length - 1, 0, "s"));
